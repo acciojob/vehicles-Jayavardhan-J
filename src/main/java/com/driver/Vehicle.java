a80d4c851348053a232da0428173edf88b3e0fad
Jayavardhan J <Jayavardhanjj3184@gmail.com>
2023-06-11 20:51:32 +0530

    Solution

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -11,6 +11,7 @@ public class Vehicle {
     private int currentDirection;
     public Vehicle(){};
     public Vehicle(String name) {
+        this.name=name;
         currentSpeed=0;
         currentDirection=0;
     }
